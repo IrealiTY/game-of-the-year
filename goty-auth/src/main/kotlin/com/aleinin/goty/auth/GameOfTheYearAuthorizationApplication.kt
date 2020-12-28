e18eec79c101127b6963f1e02d1502a59c76af18
Eric <eah0592@gmail.com>
2020-12-28 10:48:49 -0600

    #22 Initial commit of authorization server

---
+++ goty-auth/src/main/kotlin/com/aleinin/goty/auth/GameOfTheYearAuthorizationApplication.kt
@@ -0,0 +1,11 @@
+package com.aleinin.goty.auth
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GameOfTheYearAuthorizationApplication
+
+fun main(args: Array<String>) {
+	runApplication<GameOfTheYearAuthorizationApplication>(*args)
+}
---
+++ goty-auth/src/test/kotlin/com/aleinin/goty/auth/GameOfTheYearAuthorizationApplicationTests.kt
@@ -0,0 +1,11 @@
+package com.aleinin.goty.auth
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GameOfTheYearAuthorizationApplicationTests {
+
+	@Test
+	fun contextLoads() {}
+}
